ce15dd337f34401ca1256648987537ef01fa3752
Wesley Cho <57739900+wes-cho@users.noreply.github.com>
2024-02-25 22:52:29 -0800

    Clean up function that plays one round

--- scripts.js
+++ scripts.js
@@ -4,31 +4,20 @@ function getComputerChoice(){
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
-// let getComputerChoice = () => choices[Math.floor(Math.random() * choices.length)];
-
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === 'rock' && computerSelection === 'paper') {
-        alert("Your Lose! Paper beats Rock")
-    } else if (playerSelection === 'rock' && computerSelection === 'scissors'){
-        alert("You Win! Rock beats scissors!");
-    } else if (playerSelection === 'rock' && computerSelection === 'rock') {
-        alert("Tie! You both chose rock.")
-    } else if (playerSelection === 'paper' && computerSelection === 'rock'){
-        alert("You win! Paper beats rock.")
-    } else if (playerSelection === 'paper' && computerSelection === 'scissors'){
-        alert("You lose! Scissors beats paper.")
-    } else if (playerSelection === 'paper' && computerSelection === 'paper'){
-        alert("Tie! You both chose paper.")
-    } else if (playerSelection === 'scissors' && computerSelection === 'rock'){
-        alert("You lose! Rock beats scissors.")
-    } else if (playerSelection === 'scissors' && computerSelection === 'paper'){
-        alert("You win! Scissors beats paper.")
-    } else if (playerSelection = "scissors" && computerSelection === 'scissors'){
-        alert("Tie! You both chose scissors.")
-    }
+    if ((playerSelection == 'rock' && computerSelection == 'scissors')
+        || (playerSelection == 'paper' && computerSelection == 'rock')
+        || (playerSelection == 'scissors' && computerSelection == 'paper')) {
+            console.log(`You win! ${playerSelection} beats ${computerSelection}.`)
+        } else if (playerSelection === computerSelection) {
+            console.log(`Tie game. You both chose ${playerSelection}.`)
+        } else {
+            console.log(`You lost. ${computerSelection} beats ${playerSelection}.`)
+        }
 }
 
 const playerSelection = 'rock';
 const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+console.log (playRound(playerSelection, computerSelection));
+
 //
