55c8a8afc17ca1447f6295f8cfa1a8bbf27e22ab
Wesley Cho <57739900+wes-cho@users.noreply.github.com>
2024-02-26 12:28:45 -0800

    Create function that plays 5 rounds and displays overall winner

--- scripts.js
+++ scripts.js
@@ -1,10 +1,5 @@
 let choices = ['rock', 'paper', 'scissors'];
 
-let userInput = prompt('What do you choose?');
-while (userInput !== 'rock' && userInput !== 'scissors' && userInput !== 'paper'){
-    userInput = prompt('Invalid input. Choose rock, paper, or scissors.')
-}
-
 function getComputerChoice(){
     return choices[Math.floor(Math.random() * choices.length)];
 }
@@ -13,15 +8,51 @@ function playRound(playerSelection, computerSelection) {
     if ((playerSelection === 'rock' && computerSelection === 'scissors')
         || (playerSelection === 'paper' && computerSelection === 'rock')
         || (playerSelection === 'scissors' && computerSelection === 'paper')) {
-            console.log(`You win! ${playerSelection} beats ${computerSelection}.`)
+            return `You win! ${playerSelection} beats ${computerSelection}.`
         } else if (playerSelection === computerSelection) {
-            console.log(`Tie game. You both chose ${playerSelection}.`)
+            return `Tie game. You both chose ${playerSelection}.`
         } else {
-            console.log(`You lost. ${computerSelection} beats ${playerSelection}.`)
+            return `You lost. ${computerSelection} beats ${playerSelection}.`
         }
 }
 
+function playGame(){
+    //initialize score counter
+    let playerScore = 0;
+    let computerScore = 0;
 
+    //until the playerScore or cpuScore reaches 5:
+    while(playerScore < 5 && computerScore < 5){
+        //get user input & transform to lower case for later comparison
+        let userInput = prompt('What do you choose?').toLowerCase();
+
+        //ensure user input is a valid choice
+        while (userInput !== 'rock' && userInput !== 'scissors' && userInput !== 'paper'){
+            userInput = prompt('Invalid input. Choose rock, paper, or scissors.')
+        }
+        
+        //play a single round of the game
+        winLossString = playRound(userInput, getComputerChoice());
+        if (winLossString.includes('win') === true){
+            playerScore++;
+            console.log(winLossString)
+        } else if (winLossString.includes('Tie') === true){
+            playerScore = playerScore + 0;
+            console.log(winLossString)
+        } else {
+            computerScore++;
+            console.log(winLossString);
+        }
+    }
+
+    //display winner
+    if (playerScore > computerScore) {
+        console.log(`You beat the computer! You scored ${playerScore} and the computer scored ${computerScore}.`);
+    } else {
+        console.log(`You lost to the computer! You scored ${playerScore} and the computer scored ${computerScore}.`)
+    }
+}
+    
 //const playerSelection = 'rock';
 //const computerSelection = getComputerChoice();
 //console.log (playRound(playerSelection, computerSelection));
