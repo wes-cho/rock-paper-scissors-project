07cc7c60604ed106c733a942d705182a7406334c
Wesley Cho <57739900+wes-cho@users.noreply.github.com>
2024-02-25 23:20:40 -0800

    Small changes

--- scripts.js
+++ scripts.js
@@ -1,13 +1,19 @@
 let choices = ['rock', 'paper', 'scissors'];
 
+let userInput = prompt('What do you choose?').toLowerCase();
+while (userInput != choices[0] || choices[1] || choices [2]){
+    let userInput = prompt('Invalid input. Choose rock, paper, or scissors.');
+}
+console.log(userInput);
+
 function getComputerChoice(){
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
 function playRound(playerSelection, computerSelection) {
-    if ((playerSelection == 'rock' && computerSelection == 'scissors')
-        || (playerSelection == 'paper' && computerSelection == 'rock')
-        || (playerSelection == 'scissors' && computerSelection == 'paper')) {
+    if ((playerSelection === 'rock' && computerSelection === 'scissors')
+        || (playerSelection === 'paper' && computerSelection === 'rock')
+        || (playerSelection === 'scissors' && computerSelection === 'paper')) {
             console.log(`You win! ${playerSelection} beats ${computerSelection}.`)
         } else if (playerSelection === computerSelection) {
             console.log(`Tie game. You both chose ${playerSelection}.`)
@@ -18,6 +24,7 @@ function playRound(playerSelection, computerSelection) {
 
 const playerSelection = 'rock';
 const computerSelection = getComputerChoice();
+
 console.log (playRound(playerSelection, computerSelection));
 
 //
